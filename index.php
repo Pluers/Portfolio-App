c4bbbe5836453b3688157397ac615c8e0e65dfc0
Rick <S1204255@student.windesheim.nl>
2023-10-03 10:34:55 +0200

    update homepage

---
+++ index.php
@@ -0,0 +1,55 @@
+<html lang="nl">
+<link rel="stylesheet" href="style.css">
+
+<head>
+    <title>ProfielPlus</title>
+</head>
+
+<body>
+    <header>
+        <container>
+            <button>category</button>
+            <input type="text" placeholder="Search">
+            <button>Search</button>
+        </container>
+
+    </header>
+    <main>
+        <aside>
+            <button>Home</button>
+            <button>About</button>
+            <button>Profile</button>
+        </aside>
+        <gridl>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+            <p>Person</p>
+        </gridl>
+    </main>
+    <footer>
+        <p>text</p>
+    </footer>
+</body>
+
+</html>
