d25d1772498f646f12a7837060a0cbfde9c99646
Rick <S1204255@student.windesheim.nl>
2023-10-03 11:07:48 +0200

    Updated the sidebar

--- index.php
+++ index.php
@@ -16,35 +16,138 @@
     </header>
     <main>
         <aside>
-            <button>Home</button>
-            <button>About</button>
-            <button>Profile</button>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                    <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+                </svg>
+                Home
+            </button>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                    <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM169.8 165.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L280 264.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V250.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H222.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
+                </svg>
+                About
+            </button>
+            <button>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                </svg>
+                Profile
+            </button>
         </aside>
         <gridl>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
-            <p>Person</p>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
+            <article>
+                <img src="" alt="" class="square">
+                <p>Person</p>
+            </article>
         </gridl>
     </main>
     <footer>
