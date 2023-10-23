ecb7590e8e7bad99a943a3fdf7322e639186694e
Rick <S1204255@student.windesheim.nl>
2023-10-23 20:35:00 +0200

    index view fix

--- views/index.view.php
+++ views/index.view.php
@@ -7,21 +7,21 @@ require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/nav.php';
 <link rel="stylesheet" href="/views/public/styles/homepage.css">
 <section>
     <?php
-    /** @var array $users */
-    foreach ($users as $user) { ?>
+    foreach ($users as $user) {
+    ?>
         <article>
-            <div class='square'>
-                <profile>
-                    <div class='circle'>
-                        <p>
-                            <?= $user['username'] . " " . $user['isAdmin']; ?>
-                        </p>
-                        <br>
-                    </div>
-                </profile>
-            </div>
+            <img class='square'>
+            <profile>
+                <img class='circle'>
+                <p>
+                    <?= $user['username'] . " " . $user['isAdmin']; ?>
+                </p>
+                <br>
+            </profile>
         </article>
-    <?php } ?>
+    <?php
+    }
+    ?>
 </section>
 
 <?php require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/footer.php'; ?>
