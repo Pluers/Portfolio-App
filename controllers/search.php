892f0ab298177842656e901686c9a2eb7abe3e5e
Rick <S1204255@student.windesheim.nl>
2023-10-25 15:35:43 +0200

    added comments

--- controllers/search.php
+++ controllers/search.php
@@ -2,12 +2,14 @@
 function search()
 {
     if (!empty($_GET['q'])) {
+        // zorg dat je geen speciale charaters kan gebruiken
         $term = getSanitizedStr($_GET['q']);
 
         $users = customStatement("SELECT username FROM users WHERE username LIKE '%" . $term . "%'");
         $hobbies = customStatement("SELECT hobby_name FROM hobbies WHERE hobby_name LIKE '%" . $term . "%'");
         $edu = customStatement("SELECT education_name FROM educations WHERE education_name LIKE '%" . $term . "%'");
         $jobexp = customStatement("SELECT company_name, function_name FROM jobexperiences WHERE company_name LIKE '%" . $term . "%' OR function_name LIKE '%" . $term . "%'");
+        // return alle waardes zodat je ze in de view kan gebruiken
         return [
             "Users" => $users,
             "Hobbies" => $hobbies,
--- views/search.view.php
+++ views/search.view.php
@@ -6,11 +6,15 @@ require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/nav.php';
 if (!empty(search())) {
     $hasResults = false;
     foreach (search() as $title => $statementresults) {
+        // check of er resultaten zijn
         if (!empty($statementresults)) {
             $hasResults = true;
+            // display de header
             echo "<h1>" . $title . "</h1>";
+            // loop door de multidimensional array
             foreach ($statementresults as $results) {
                 foreach ($results as $result) {
+                    // display de results
                     echo $result . "<br>";
                 }
             }
