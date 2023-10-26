df977aa76de6bf6904acbff814b7e0a3002be9f1
Rick <S1204255@student.windesheim.nl>
2023-10-26 16:28:59 +0200

    changed the editprofile page css

--- controllers/edit_profile.php
+++ controllers/edit_profile.php
@@ -2,7 +2,7 @@
 
 function editUserInfo()
 {
-    $_SESSION['users_id'] = 3;
+    $_SESSION['users_id'] = 2;
     if (isset($_POST['edituser'])) {
         customStatement("UPDATE users SET username = '" . $_POST['username'] . "' WHERE " . $_SESSION['users_id'] . " = users.users_id");
         customStatement("UPDATE users SET first_name = '" . $_POST['firstName'] . "' WHERE " . $_SESSION['users_id'] . " = users.users_id");
@@ -27,6 +27,20 @@ function updateUserInfo()
         "email" => loopGetinfo(customStatement("SELECT email FROM users WHERE " . $_SESSION['users_id'] . " = users.users_id")),
     ];
 }
+if (isset($_POST['upload'])) {
+    $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/views/public/images/";
+    $user_id = $_SESSION['users_id'];
+    $target_file = $target_dir . "profile_picture_" . $user_id . ".jpg";
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $extensions_arr = array("jpg", "jpeg", "png", "gif");
+
+    if (in_array($imageFileType, $extensions_arr)) {
+        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+            $_SESSION['profile_picture'] = "profile_picture_" . $user_id . ".jpg";
+        }
+    }
+}
+
 
 
 
