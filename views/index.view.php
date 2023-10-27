c312fa2493ab9f8f85d8ac006d45e2fd429fd437
Rick <S1204255@student.windesheim.nl>
2023-10-27 15:19:59 +0200

    updated view index temp

--- views/index.view.php
+++ views/index.view.php
@@ -10,14 +10,13 @@ require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/nav.php';
     foreach ($users as $user) {
     ?>
         <article>
-            <img class='square'>
-            <profile>
-                <img src=<?= file_exists($target_dir_img . "profile_picture_" . $user['users_id'] . ".jpg") ? "/views/public/images/profile_picture_" . $user['users_id'] . ".jpg" : "/views/public/images/default.png" ?>>
-                <p>
-                    <?= $devmode ? (!empty($user['username']) ? "username: " . $user['username'] . " | " : "") . $user['first_name'] . " " . $user['last_name'] . " <b>isAdmin: " . $user['isAdmin'] . "</b>" : $user['first_name'] . " " . $user['last_name']; ?>
-                </p>
-                <br>
-            </profile>
+            <img src=<?= file_exists($target_dir_img . "profile_picture_" . $user['users_id'] . ".jpg") ? "/views/public/images/profile_picture_" . $user['users_id'] . ".jpg" : "/views/public/images/default.png" ?>>
+            <h1>
+                <?= $devmode ? (!empty($user['username']) ? "username: " . $user['username'] . " | " : "") . $user['first_name'] . " " . $user['last_name'] . " <b>isAdmin: " . $user['isAdmin'] . "</b>" : $user['first_name'] . " " . $user['last_name']; ?>
+            </h1>
+            <p>
+                <!-- print bio of user -->
+            </p>
         </article>
     <?php
     }
