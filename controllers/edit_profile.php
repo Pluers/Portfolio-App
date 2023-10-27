7d8ff45d3a942e2e1be8a093db5cd682ca38be2c
Rick <S1204255@student.windesheim.nl>
2023-10-27 12:48:40 +0200

    updated editprofile
    still not working yet

--- controllers/edit_profile.php
+++ controllers/edit_profile.php
@@ -1,31 +1,35 @@
 <?php
 function getUserInfo()
 {
-    echo $_SESSION['users_id'];
-    $getUsers = customStatement("SELECT * FROM users WHERE users_id = :users_id", [':users_id' => $_SESSION['users_id']]);
-    return $getUsers;
+    echo $_SESSION['user_id'] = 1;
+    // return var_dump(customStatement("SELECT first_name, last_name, email from users WHERE users_id = '" . $_SESSION['user_id'] . "'", ""));
+    return [
+        "first_name" => customStatement("SELECT first_name FROM users WHERE users_id = " . $_SESSION['users_id'], ""),
+        "last_name" => customStatement("SELECT last_name FROM users WHERE users_id = " . $_SESSION['users_id'], ""),
+        "email" => customStatement("SELECT email FROM users WHERE users_id = " . $_SESSION['users_id'], ""),
+    ];
 }
 
-$_SESSION['users_id'] = 1;
+$_SESSION['user_id'] = 1;
 $target_dir = $_SERVER['DOCUMENT_ROOT'] . "/views/public/images/";
-$users_id = $_SESSION['users_id'];
+$user_id = $_SESSION['user_id'];
 if (isset($_POST['edituser'])) {
-    customStatement("UPDATE users SET first_name = '" . $_POST['firstName'] . "', last_name = '" . $_POST['lastName'] . "', email= '" . $_POST['email'] . "' WHERE users_id = :users_id", [':users_id' => $users_id]);
+    customStatement("UPDATE users SET first_name = '" . $_POST['firstName'] . "', last_name = '" . $_POST['lastName'] . "', email= '" . $_POST['email'] . "' WHERE users_id = :user_id", [':user_id' => $user_id]);
 }
 // check if profile picture exists
-if (file_exists($target_dir . "profile_picture_" . $users_id . ".jpg")) {
-    $profileimg = "profile_picture_" . $users_id . ".jpg";
+if (file_exists($target_dir . "profile_picture_" . $user_id . ".jpg")) {
+    $profileimg = "profile_picture_" . $user_id . ".jpg";
 } else {
     $profileimg = "default.png";
 }
 if (isset($_POST['uploadpfp'])) {
-    $target_file = $target_dir . "profile_picture_" . $users_id . ".jpg";
+    $target_file = $target_dir . "profile_picture_" . $user_id . ".jpg";
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
     $extensions_arr = array("jpg", "jpeg", "png", "gif");
 
     if (in_array($imageFileType, $extensions_arr)) {
         if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
-            $_SESSION['profile_picture'] = "profile_picture_" . $users_id . ".jpg";
+            $_SESSION['profile_picture'] = "profile_picture_" . $user_id . ".jpg";
         }
     }
 }
--- views/editprofile.view.php
+++ views/editprofile.view.php
@@ -22,7 +22,7 @@ require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/nav.php';
     </form>
     <form method="post">
         <label for="FirstName">First name:</label>
-        <input type="text" placeholder="First Name" name="firstName" value="<?php var_dump(getUserInfo()["first_name"])  ?>"> <br>
+        <input type="text" placeholder="First Name" name="firstName" value="<?= getUserInfo()["first_name"]["first_name"] ?>"> <br>
         <label for="LastName">Last name:</label>
         <input type="text" placeholder="Last Name" name="lastName" value="<?= getUserInfo()["last_name"] ?>"> <br>
         <label for="Email">Email:</label>
