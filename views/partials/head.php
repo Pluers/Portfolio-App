436007cce27689c48c57583e0f070c4da78f2ae4
Rick <S1204255@student.windesheim.nl>
2023-10-28 15:15:45 +0200

    added favicon

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,9 +4,9 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ProfielPlus</title>
-    <link rel="icon" type="image/x-icon" href="https://twinery.org/cookbook/stylesheets/logo.svg"/>
+    <link rel="icon" type="image/x-icon" href="/views/public/favicon.png" />
     <!-- CSS for the index -->
-    <link rel="stylesheet" href="/views/public/styles/style.css"/>
+    <link rel="stylesheet" href="/views/public/styles/style.css" />
     <!-- Link the icons from Google -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
