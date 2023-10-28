f8fbdaf4488d9b410afe907a3394438b7216bdc0
Rick <S1204255@student.windesheim.nl>
2023-10-28 16:44:29 +0200

    corrected the display info

--- controllers/profile.php
+++ controllers/profile.php
@@ -1,26 +1,9 @@
 <?php
-
-
-
-
-
-
-
-$_SESSION['user_id'] = 3;
-
-
-$user_id = $_SESSION['user_id'];
-
- 
-
-
-
 function getUserInfo()
 {
     global $conn;
-    $user_id = $_SESSION['user_id'];
     $stmt = $conn->prepare("SELECT * FROM users WHERE users_id = :user_id");
-    $stmt->execute([':user_id' => $user_id]);
+    $stmt->execute([':user_id' => $_SESSION['user_id']]);
     $user_info = $stmt->fetch(PDO::FETCH_ASSOC);
     if ($user_info) {
         return $user_info;
@@ -28,22 +11,10 @@ function getUserInfo()
         // handle the case where no rows are returned by the SQL statement
     }
 }
-    
-        
-    
 
-
-
-
-if (file_exists($target_dir_img . "profile_picture_" . $user_id . ".jpg")) {
-    $profileimg = "profile_picture_" . $user_id . ".jpg";
+if (file_exists($target_dir_img . "profile_picture_" . $_SESSION['user_id'] . ".jpg")) {
+    $profileimg = "profile_picture_" . $_SESSION['user_id'] . ".jpg";
 } else {
     $profileimg = "default.png";
 }
-
-
-
-
-
-
-    require $_SERVER['DOCUMENT_ROOT'] . '/views/profile.view.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/views/profile.view.php';
