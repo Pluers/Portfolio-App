2d0b121956c4049f284b2233453a5ddc97e94dde
Rick <S1204255@student.windesheim.nl>
2023-10-28 14:51:50 +0200

    - added separate pages -

--- controllers/edit_profile.php
+++ controllers/edit_profile.php
@@ -1,8 +1,77 @@
 <?php
+function profilePage()
+{
+    global $target_dir_img;
+    $_SESSION['user_id'] = 1;
+    $user_id = $_SESSION['user_id'];
+    if (isset($_POST['edituser'])) {
+        customStatement("UPDATE users SET first_name = '" . $_POST['first_name'] . "', last_name = '" . $_POST['last_name'] . "', email= '" . $_POST['email'] . "' WHERE users_id = :user_id", [':user_id' => $user_id]);
+    }
+    // check if profile picture exists
+    if (file_exists($target_dir_img . "profile_picture_" . $user_id . ".jpg")) {
+        $profileimg = "profile_picture_" . $user_id . ".jpg";
+    } else {
+        $profileimg = "default.png";
+    }
+    if (isset($_POST['uploadpfp'])) {
+        $target_file = $target_dir_img . "profile_picture_" . $user_id . ".jpg";
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        $extensions_arr = array("jpg", "jpeg", "png", "gif");
+
+        if (in_array($imageFileType, $extensions_arr)) {
+            if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+                $_SESSION['profile_picture'] = "profile_picture_" . $user_id . ".jpg";
+            }
+        }
+    }
+?>
+    <contentsection>
+        <h1> Edit Profile</h1>
+        <form method="post" enctype="multipart/form-data" class="setprofilepicture">
+            <img src="/views/public/images/<?= $profileimg ?>" />
+            <label for="fileToUpload">
+                <!-- hidden file input that gets replaced by the span -->
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*" /><br>
+                <span>Select Image
+                    <span class="material-symbols-rounded">
+                        add_photo_alternate
+                    </span>
+                </span>
+            </label>
+            <input type="submit" value="Upload" name="uploadpfp">
+        </form>
+        <form method="post" class="editprofile">
+            <label for="first_name">First name:</label>
+            <input type="text" placeholder="First Name" name="first_name" value="<?= getUserInfo()["first_name"] ?>">
+            <label for="last_name">Last name:</label>
+            <input type="text" placeholder="Last Name" name="last_name" value="<?= getUserInfo()["last_name"] ?>">
+            <label for="email">Email:</label>
+            <input type="text" Placeholder="Email" name="email" value="<?= getUserInfo()["email"] ?>">
+            <label for="change_password">Password: </label>
+            <a href="/forgot" target="_blank">Change Password
+                <span class="material-symbols-rounded">
+                    open_in_new
+                </span>
+            </a>
+            <input type="submit" value="Submit" name="edituser">
+        </form>
+    </contentsection>
+<?php
+}
 function aboutPage()
 {
 ?>
-    <section>
+    <contentsection>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus iusto obcaecati nobis possimus at ducimus sint, facere voluptatem iure, nam illum tempora assumenda hic. Voluptatum cum earum totam magni mollitia!
+        </p>
+    </contentsection>
+<?php
+}
+function hobbiesPage()
+{
+?>
+    <contentsection>
         <form action="">
             <select name="hobbies" id="">
                 <option value="hobby1">Hobby 1</option>
@@ -27,10 +96,9 @@ function aboutPage()
             </select>
             <input type="submit" value="Add Hobby"></input>
         </form>
-    </section>
+    </contentsection>
 <?php
 }
-
 function getUserInfo()
 {
     global $conn;
@@ -44,33 +112,4 @@ function getUserInfo()
         // handle the case where no rows are returned by the SQL statement
     }
 }
-
-$_SESSION['user_id'] = 1;
-$user_id = $_SESSION['user_id'];
-if (isset($_POST['edituser'])) {
-    customStatement("UPDATE users SET first_name = '" . $_POST['first_name'] . "', last_name = '" . $_POST['last_name'] . "', email= '" . $_POST['email'] . "' WHERE users_id = :user_id", [':user_id' => $user_id]);
-}
-// check if profile picture exists
-if (file_exists($target_dir_img . "profile_picture_" . $user_id . ".jpg")) {
-    $profileimg = "profile_picture_" . $user_id . ".jpg";
-} else {
-    $profileimg = "default.png";
-}
-if (isset($_POST['uploadpfp'])) {
-    $target_file = $target_dir_img . "profile_picture_" . $user_id . ".jpg";
-    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-    $extensions_arr = array("jpg", "jpeg", "png", "gif");
-
-    if (in_array($imageFileType, $extensions_arr)) {
-        if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
-            $_SESSION['profile_picture'] = "profile_picture_" . $user_id . ".jpg";
-        }
-    }
-}
-
-
-
-
-
-
 require $_SERVER['DOCUMENT_ROOT'] . '/views/editprofile.view.php';
--- views/editprofile.view.php
+++ views/editprofile.view.php
@@ -7,64 +7,33 @@ require $_SERVER['DOCUMENT_ROOT'] . '/views/partials/nav.php';
 <!-- input fields and edit buttons for all categories below -->
 
 <section>
-    <h1> Edit Profile</h1>
-    <form method="post" enctype="multipart/form-data" class="setprofilepicture">
-        <img src="/views/public/images/<?= $profileimg ?>" />
-        <label for="fileToUpload">
-            <!-- hidden file input that gets replaced by the span -->
-            <input type="file" name="fileToUpload" id="fileToUpload" accept="image/*" /><br>
-            <span>Select Image
-                <span class="material-symbols-rounded">
-                    add_photo_alternate
-                </span>
-            </span>
-        </label>
-        <input type="submit" value="Upload" name="uploadpfp">
-    </form>
-    <form method="post" class="editprofile">
-        <label for="first_name">First name:</label>
-        <input type="text" placeholder="First Name" name="first_name" value="<?= getUserInfo()["first_name"] ?>">
-        <label for="last_name">Last name:</label>
-        <input type="text" placeholder="Last Name" name="last_name" value="<?= getUserInfo()["last_name"] ?>">
-        <label for="email">Email:</label>
-        <input type="text" Placeholder="Email" name="email" value="<?= getUserInfo()["email"] ?>">
-        <label for="change_password">Password: </label>
-        <a href="/forgot" target="_blank">Change Password
-            <span class="material-symbols-rounded">
-                open_in_new
-            </span>
-        </a>
-        <input type="submit" value="Submit" name="edituser">
-    </form>
-</section>
-<section>
 
     <?php
-    $active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'about';
+    $active_tab = isset($_GET['tab']) ? $_GET['tab'] : '';
     ?>
 
-    <main>
+    <nav>
         <nav>
             <ul>
-                <li><a href="?tab=about" <?php if ($active_tab === 'about') {
-                                                echo 'class="active"';
-                                            } ?>>About</a></li>
-                <li><a href="?tab=attributes" <?php if ($active_tab === 'attributes') {
-                                                    echo 'class="active"';
-                                                } ?>>Attributes</a></li>
-                <li><a href="?tab=more-info" <?php if ($active_tab === 'more-info') {
-                                                    echo 'class="active"';
-                                                } ?>>More Info</a></li>
+                <li><a href="?tab=profile" <?php echo $active_tab === 'profile' ? 'class="active"' : ''; ?>>Profile</a></li>
+                <li><a href="?tab=about" <?php echo $active_tab === 'about' ? 'class="active"' : ''; ?>>About</a></li>
+                <li><a href="?tab=hobbies" <?php echo $active_tab === 'hobbies' ? 'class="active"' : ''; ?>>Hobbies</a></li>
+                <li><a href="?tab=jobs" <?php echo $active_tab === 'jobs' ? 'class="active"' : ''; ?>>Job experiences</a></li>
+                <li><a href="?tab=educations" <?php echo $active_tab === 'educations' ? 'class="active"' : ''; ?>>Educations</a></li>
             </ul>
         </nav>
-        <?php if ($active_tab === 'about') {
-            aboutPage();
-        } elseif ($active_tab === 'attributes') {
-            // code for the 'attributes' tab
-        } elseif ($active_tab === 'more-info') {
-            // code for the 'more-info' tab
-        } ?>
-    </main>
+    </nav>
+    <?php
+    if ($active_tab === 'profile') {
+        profilePage();
+    } elseif ($active_tab === 'about') {
+        aboutPage();
+    } elseif ($active_tab === 'hobbies') {
+        hobbiesPage();
+    } elseif ($active_tab === 'jobs') {
+    } elseif ($active_tab === 'educations') {
+    }
+    ?>
 
 
 </section>
