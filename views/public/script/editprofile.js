d377706b63299af299e59736790f06168a2085ba
Rick <S1204255@student.windesheim.nl>
2023-10-30 20:26:26 +0100

    updated js
    for a preview image

--- views/public/script/editprofile.js
+++ views/public/script/editprofile.js
@@ -1,14 +1,20 @@
 window.onload = () => {
     let uploadImgInput = document.querySelector("input[type=file][name=imgToUpload]");
     let newProfileImg = document.querySelector("img[name=newProfileImg]");
+    let previewImg = document.querySelectorAll("images span.material-symbols-rounded, images img[name = newProfileImg]");
 
     uploadImgInput.addEventListener("change", () => {
-        let file = uploadImgInput.files[0];
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        console.log(reader);
-        reader.onload = () => {
-            newProfileImg.src = reader.result;
-        };
+        if (uploadImgInput.files.length > 0) {
+            let file = uploadImgInput.files[0];
+            let reader = new FileReader();
+            reader.readAsDataURL(file);
+            console.log(reader);
+            reader.onload = () => {
+                newProfileImg.src = reader.result;
+            };
+            previewImg.forEach(img => img.style.display = "unset");
+        } else {
+            previewImg.forEach(img => img.style.display = "none");
+        }
     });
 }
