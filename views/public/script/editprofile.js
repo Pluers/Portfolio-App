ce35f397f07b121c4cdfc1d28659b5b74935205f
Rick <S1204255@student.windesheim.nl>
2023-10-30 20:50:01 +0100

    simple js changes
    - also added back functions

--- views/public/script/editprofile.js
+++ views/public/script/editprofile.js
@@ -1,17 +1,17 @@
 window.onload = () => {
-    let uploadImgInput = document.querySelector("input[type=file][name=imgToUpload]");
-    let newProfileImg = document.querySelector("img[name=newProfileImg]");
-    let previewImg = document.querySelectorAll("images span.material-symbols-rounded, images img[name = newProfileImg]");
+    const uploadImgInput = document.querySelector("input[type=file][name=imgToUpload]");
+    const newProfileImg = document.querySelector("img[name=newProfileImg]");
+    const previewImg = document.querySelectorAll("images span.material-symbols-rounded, images img[name = newProfileImg]");
+    const reader = new FileReader();
+
+    reader.onload = () => {
+        newProfileImg.src = reader.result;
+    };
 
     uploadImgInput.addEventListener("change", () => {
         if (uploadImgInput.files.length > 0) {
-            let file = uploadImgInput.files[0];
-            let reader = new FileReader();
+            const file = uploadImgInput.files[0];
             reader.readAsDataURL(file);
-            console.log(reader);
-            reader.onload = () => {
-                newProfileImg.src = reader.result;
-            };
             previewImg.forEach(img => img.style.display = "unset");
         } else {
             previewImg.forEach(img => img.style.display = "none");
--- views/public/script/editprofilehobbies.js
+++ views/public/script/editprofilehobbies.js
@@ -1,19 +1,23 @@
 window.onload = () => {
-    let hobbiesSelect = document.getElementById('hobbySelection');
-    let createHobbySection = document.getElementById('createHobbyForm');
-    let addHobbyButton = document.querySelector("input[name=add_hobby_to_profile]");
-    let createHobbyButton = document.querySelector("input[name=create_hobby]");
-    let importImage = document.querySelector("form[name=import_image]");
+    const hobbiesSelect = document.getElementById('hobbySelection');
+    const createHobbySection = document.getElementById('createHobbyForm');
+    const addHobbyButton = document.querySelector("input[name=add_hobby_to_profile]");
+    const createHobbyButton = document.querySelector("input[name=create_hobby]");
+    const importImage = document.querySelector("form[name=import_image]");
+
     hobbiesSelect.addEventListener('change', () => {
-        if (hobbiesSelect.value === "0") {
-            addHobbyButton.disabled = true;
-        } else if (hobbiesSelect.value === 'create_new_hobby') {
-            createHobbySection.style.display = 'flex';
-            addHobbyButton.style.display = 'none';
-        } else {
-            createHobbySection.style.display = 'none';
-            addHobbyButton.style.display = 'flex';
-            addHobbyButton.disabled = false;
+        switch (hobbiesSelect.value) {
+            case "0":
+                addHobbyButton.disabled = true;
+                break;
+            case "create_new_hobby":
+                createHobbySection.style.display = 'flex';
+                addHobbyButton.style.display = 'none';
+                break;
+            default:
+                createHobbySection.style.display = 'none';
+                addHobbyButton.style.display = 'flex';
+                addHobbyButton.disabled = false;
         }
     });
 }
