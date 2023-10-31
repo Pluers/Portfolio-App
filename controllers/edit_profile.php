abb6e1f8ed90d02419efef0236df483238289866
Rick <S1204255@student.windesheim.nl>
2023-10-31 15:41:13 +0100

    change edituser

--- controllers/edit_profile.php
+++ controllers/edit_profile.php
@@ -19,13 +19,13 @@ function profilePage()
         <h1> Edit Profile</h1>
         <form method="post" enctype="multipart/form-data" class="setprofilepicture">
             <images>
-                <img src="/views/public/images/<?= $profileimg ?>"/>
+                <img src="/views/public/images/<?= $profileimg ?>" />
                 <span class="material-symbols-rounded"> </span>
-                <img src="/views/public/images/<?= $profileimg ?>" name="newProfileImg"/>
+                <img src="/views/public/images/<?= $profileimg ?>" name="newProfileImg" />
             </images>
             <label for="imgToUpload" class="uploadImage">
                 <!-- hidden file input that gets replaced by the span -->
-                <input type="file" name="imgToUpload" id="imgToUpload" accept="image/*"/>
+                <input type="file" name="imgToUpload" id="imgToUpload" accept="image/*" />
                 <span>Select Image
                     <span class="material-symbols-rounded">
                         add_photo_alternate
@@ -48,8 +48,7 @@ function profilePage()
                 </span>
             </a>
             <label for="description">Description / Biography:</label>
-            <textarea name="description" id="" rows="4" name="description"
-                      placeholder="Empty description"><?= getUserInfo()["description"] ?></textarea>
+            <textarea name="description" id="" rows="4" name="description" placeholder="Empty description"><?= getUserInfo()["description"] ?></textarea>
             <input type="submit" value="Submit" name="edituser">
         </form>
     </contentsection>
@@ -58,16 +57,16 @@ function profilePage()
 
 function hobbiesPage()
 {
-    global $target_dir_img, $hobbies, $hobbyimg, $hobby_name;
+    global $target_dir_img, $hobbies, $hobbyimg, $hobby_name, $user_hobbies;
 
-    if (isset($_POST['uploadImgSubmit'])) {
+    if (isset($_POST['create_hobby'])) {
         $target_file = $target_dir_img . "hobby_" . $hobby_name . ".jpg";
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
         $extensions_arr = array("jpg", "jpeg", "png", "gif");
 
         if (in_array($imageFileType, $extensions_arr)) {
-            if (move_uploaded_file($_FILES['hobbyImgUpload']['tmp_name'], $target_file)) {
-                $hobbyimg = "profile_picture_" . $hobby_name . ".jpg";
+            if (move_uploaded_file($_FILES['imgToUpload']['tmp_name'], $target_file)) {
+                $hobbyimg = "hobby_" . $hobby_name . ".jpg";
             }
         }
     }
@@ -75,8 +74,8 @@ function hobbiesPage()
     <contentsection>
         <h1>Add Hobbies</h1>
         <!-- hobby selector -->
-        <form method="post" id="hobbies">
-            <select name="hobbies" id="hobbySelection">
+        <form method="post" id="hobbiesForm">
+            <select name="hobbiesList" id="hobbySelection">
                 <option value="0" name='default' selected disabled>Select a hobby</option>
                 <?php
                 foreach ($hobbies as $hobby) {
@@ -87,35 +86,39 @@ function hobbiesPage()
             </select>
             <input type="submit" value="Add hobby" name="add_hobby_to_profile">
         </form>
-        <img src="/views/public/images/<?= $hobbyimg ?>" alt="">
-        <!-- upload image to hobby -->
-        <form method="post" class="uploadImage">
-            <label for="hobbyImgUpload">
+
+        <!-- create new hobby -->
+        <form method="post" id="createHobbyForm" enctype="multipart/form-data" style="display: none;">
+            <label for="imgToUpload" class="uploadImage">
                 <!-- hidden file input that gets replaced by the span -->
-                <input type="file" name="hobbyImgUpload" id="hobbyImgUpload" accept="image/*" />
-                <input type="file" name="imgToUpload" id="imgToUpload" accept="image/*"/>
+                <input type="file" name="imgToUpload" id="imgToUpload" accept="image/*" />
                 <span>Select Image
                     <span class="material-symbols-rounded">
                         add_photo_alternate
                     </span>
                 </span>
             </label>
-            <input type="submit" name="uploadImgSubmit"/>
-        </form>
-        <!-- create new hobby -->
-        <form method="post" id="createHobbyForm" style="display: none;">
+            <!-- input text -->
             <input type="text" placeholder="Enter new hobby name" name="create_hobby_name">
             <input type="submit" value="Create hobby" name="create_hobby">
+
         </form>
 
         <hobbygrid>
             <!-- display the hobbies that the user has selected -->
-            <hobbyarticle>
-                <img src="" alt="">
-            </hobbyarticle>
+            <?php
+            foreach ($user_hobbies as $user_hobby) {
+            ?>
+                <hobbyarticle>
+                    <img src="/views/public/images/hobby_<?= $user_hobby['hobby_name'] ?>.jpg" alt="">
+                    <p><?= $user_hobby['hobby_name'] ?></p>
+                </hobbyarticle>
+            <?php
+            }
+            ?>
         </hobbygrid>
     </contentsection>
-    <?php
+<?php
 }
 
 // functie om de user informatie op te halen.
@@ -134,7 +137,8 @@ function getUserInfo()
 // variables 
 $user_id = $_SESSION[SESSION_KEY_USER_ID];
 $hobbies = customStatement("SELECT * FROM hobbies");
-$hobby_name = isset($_POST['hobbies']) ? $_POST['hobbies'] : "default";
+$user_hobbies = customStatement("SELECT * FROM hobbies JOIN user_hobbies WHERE user_hobbies.users_id = :user_id", [':user_id' => $user_id]);
+$hobby_name = isset($_POST['create_hobby_name']) ? $_POST['create_hobby_name'] : "default";
 
 // check if profile picture exists
 if (file_exists($target_dir_img . "profile_picture_" . $user_id . ".jpg")) {
@@ -159,6 +163,7 @@ if (isset($_POST['edituser'])) {
         }
     }
 } else if (isset($_POST["create_hobby"])) {
+    // maak hobby aan en zet het in de rij van de hobbies die je kan selecteren
     customStatement("INSERT IGNORE INTO hobbies (hobby_name) VALUE (:hobby_name)", [':hobby_name' => ucfirst($_POST['create_hobby_name'])]);
 }
 
